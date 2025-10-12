40cb5f8be204bd5b75d731150056c1e6355225c6
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2025-10-12 14:24:35 +0300

    Update Movies.jsx

--- src/Components/Movies/Movies.jsx
+++ src/Components/Movies/Movies.jsx
@@ -6,18 +6,31 @@ const Movies = () => {
   const API_KEY = "d4b6bc723ac291b078823a9b64bd3e08";
 
   const [movies, setMovies] = useState([]);
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
 
   useEffect(() => {
     axios
-      .get(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`)
+      .get(
+        `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&page=${page}`
+      )
       .then((res) => {
         setMovies(res.data.results);
+        setTotalPages(Math.min(res.data.total_pages, 20));
         // console.log(res.data.results);
       })
       .catch((err) => {
         console.log(err);
       });
-  }, []);
+  }, [page]);
+
+  const nextPage = () => {
+    if (page < totalPages) setPage((prev) => prev + 1);
+  };
+
+  const prevPage = () => {
+    if (page > 1) setPage((prev) => prev - 1);
+  };
 
   return (
     <div className="container mx-auto my-auto p-6 mt-10 bg-[#132440] text-white shadow-md">
@@ -34,6 +47,28 @@ const Movies = () => {
           />
         ))}
       </ul>
+
+      <div className="flex justify-center items-center gap-4 mt-8">
+        <button
+          onClick={prevPage}
+          disabled={page === 1}
+          className="px-4 py-2 bg-gray-700 rounded hover:bg-gray-600 disabled:opacity-40"
+        >
+          {"<<"}
+        </button>
+
+        <span className="text-lg font-medium">
+          Page {page} of {totalPages}
+        </span>
+
+        <button
+          onClick={nextPage}
+          disabled={page === totalPages}
+          className="px-4 py-2 bg-blue-600 rounded hover:bg-blue-700 disabled:opacity-40"
+        >
+          {">>"}
+        </button>
+      </div>
     </div>
   );
 };
