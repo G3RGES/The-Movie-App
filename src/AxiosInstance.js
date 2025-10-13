e18a60fd870ec72d1576cddf7d48570fbf95049a
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2025-10-13 11:18:09 +0300

    Update AxiosInstance.js

--- src/AxiosInstance.js
+++ src/AxiosInstance.js
@@ -10,3 +10,21 @@ export const axiosInstance = axios.create({
     api_key: API_KEY,
   },
 });
+
+// axiosInstance.interceptors.request.use(
+//   (config) => {
+//     if (config.baseURL == "/movie/popular" && config.method == "get") {
+//       config.headers.Authorization = `Bearer ${API_KEY}`;
+//     }
+//     console.log(config);
+//     // return config;
+//   },
+//   (error) => {
+//     return Promise.reject(error);
+//   }
+// );
+
+// axiosInstance.interceptors.response.use(
+//   (response) => response,
+//   (error) => Promise.reject(error)
+// );
