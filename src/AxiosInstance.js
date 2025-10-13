4ac7e580bca4bc29370ed792f06406935068b87a
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2025-10-13 11:55:58 +0300

    Update AxiosInstance.js

--- src/AxiosInstance.js
+++ src/AxiosInstance.js
@@ -2,15 +2,11 @@ import axios from "axios";
 
 const API_KEY = "d4b6bc723ac291b078823a9b64bd3e08";
 
-export const axiosInstance = axios.create({
+const axiosInstance = axios.create({
   baseURL: "https://api.themoviedb.org/3",
-  timeout: 2000,
-  params: {
-    // api_key: "d4b6bc723ac291b078823a9b64bd3e08",
-    // api_key: API_KEY,
-  },
+  //   timeout: 2000,
 });
-
+export default axiosInstance;
 // axiosInstance.interceptors.request.use(
 //   (config) => {
 //     if (config.baseURL == "/movie/popular" && config.method == "get") {
