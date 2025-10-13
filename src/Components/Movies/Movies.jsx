4fa8bc82946c52b0c7f92b24d52fd3d0706b40c6
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2025-10-13 11:18:07 +0300

    Update Movies.jsx

--- src/Components/Movies/Movies.jsx
+++ src/Components/Movies/Movies.jsx
@@ -1,5 +1,6 @@
 // import axios from "axios";
-import React, { useEffect, useState } from "react";
+// import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Movie from "./Movie";
 import { useLoaderData, useNavigate, useRouteError } from "react-router-dom";
 import { axiosInstance } from "../../AxiosInstance";
