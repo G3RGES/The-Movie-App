3d803c1d2cc1cdef1c7bc055da68840471041946
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2025-10-13 11:35:25 +0300

    Update AxiosInstance.js

--- src/AxiosInstance.js
+++ src/AxiosInstance.js
@@ -7,7 +7,7 @@ export const axiosInstance = axios.create({
   timeout: 2000,
   params: {
     // api_key: "d4b6bc723ac291b078823a9b64bd3e08",
-    api_key: API_KEY,
+    // api_key: API_KEY,
   },
 });
 
