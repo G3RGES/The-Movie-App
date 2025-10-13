47ae2ed2b0b5d8f6066a9c4e72256ba9404a4b1e
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2025-10-13 09:52:19 +0300

    Create AxiosInstance.js

---
+++ src/AxiosInstance.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const API_KEY = "d4b6bc723ac291b078823a9b64bd3e08";
+
+axios.create({
+  baseURL: "https://api.themoviedb.org/3/movie/popular",
+  timeout: 2000,
+  params: {
+    // api_key: "d4b6bc723ac291b078823a9b64bd3e08",
+    api_key: API_KEY,
+  },
+});
