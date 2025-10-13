a1308d460a0136be0dced39698eb66fbdc8e6f53
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2025-10-13 09:38:23 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+// import React, { useEffect, useState } from "react";
 import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
 import Navbar from "./Components/Nav/Navbar";
 import Movies from "./Components/Movies/Movies";
@@ -6,8 +6,20 @@ import MovieDetails from "./Components/Movies/MovieDetails";
 import SearchResults from "./Components/Search/SearchResults";
 
 const App = () => {
+  // const [dark, setDark] = useState(false);
+
+  // useEffect(() => {
+  //   if (dark) {
+  //     document.documentElement.classList.add("dark");
+  //   } else {
+  //     document.documentElement.classList.remove("dark");
+  //   }
+  // }, [dark]);
+
   return (
-    <div className="bg-gray-900 text-white min-h-screen">
+    <div
+      className={`dark:bg-gray-900 dark:text-gray-100 bg-gray-100 text-gray-900 min-h-screen`}
+    >
       {/* <BrowserRouter>
         <Navbar />
 
@@ -19,6 +31,7 @@ const App = () => {
         </Routes>
       </BrowserRouter> */}
 
+      {/* <Navbar darkMode={dark} setDark={setDark} /> */}
       <Navbar />
       <div className="pt-20 px-6">
         <Outlet />
