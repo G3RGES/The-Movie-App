64d42dd6ad253f3f79fdb1a57beffe890ab128bc
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2025-10-14 10:29:50 +0300

    Update MainComponent.jsx

--- src/Components/MainComponent/MainComponent.jsx
+++ src/Components/MainComponent/MainComponent.jsx
@@ -1,20 +1,39 @@
 import React from "react";
 import Navbar from "../Nav/Navbar";
 import { Outlet } from "react-router-dom";
-import { toggleTheme } from "../../store/themeSlice";
-// import { useSelector, connect, useDispatch } from "react-redux";
+import { toggleThemeMode } from "../../store/themeSlice";
+
 import { useSelector, useDispatch } from "react-redux";
+import { changeLang } from "../../store/LangSlice";
 
 const MainComponent = () => {
   const theme = useSelector((state) => state.theme.mode);
+  const lang = useSelector((state) => state.lang.lang);
+
+  console.log(theme);
+  console.log(lang);
+
   // const theme = useSelector((state) => state.theme);
   const dispatch = useDispatch();
 
+  // const setLang = () => {
+  //   dispatch(changeLang(lang === "en" ? "ar" : "en"));
+  // };
+
+  // const toggleTheme = () => {
+  //   dispatch(toggleThemeMode());
+  // };
+
   return (
     <>
-      <Navbar />
-      <div className="pt-20 px-6">
-        <Outlet />
+      <div
+        className={`${theme === "dark" ? "dark" : ""}`}
+        dir={lang === "ar" ? "rtl" : "ltr"}
+      >
+        <Navbar />
+        <div className="pt-20 px-6">
+          <Outlet />
+        </div>
       </div>
     </>
   );
