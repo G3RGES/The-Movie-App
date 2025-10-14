d3310f59cb112480ca9e3cc580deb14fd7b73514
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2025-10-14 11:24:29 +0300

    Update store.js

--- src/store/store.js
+++ src/store/store.js
@@ -2,9 +2,11 @@ import { configureStore } from "@reduxjs/toolkit";
 // import { createStore } from "@reduxjs/toolkit";
 import themeReducer from "./themeSlice";
 import LangReducer from "./LangSlice";
+import LoaderSlice from "./LoaderSlice";
 export const store = configureStore({
   reducer: {
     theme: themeReducer,
     lang: LangReducer,
+    loader: LoaderSlice,
   },
 });
