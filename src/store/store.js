20d0dec388a117c11ccfd4633e593ff3d4543cdb
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2025-10-14 10:08:39 +0300

    Update store.js

--- src/store/store.js
+++ src/store/store.js
@@ -8,28 +8,3 @@ export const store = configureStore({
     lang: LangReducer,
   },
 });
-
-// const themeReducer = (state = { theme: "light" }, action) => {
-//   if (action.type === "SET_THEME") {
-//     return {
-//       theme: action.payload,
-//     };
-//   }
-
-//   if (action.type === "TOGGLE_THEME") {
-//     return {
-//       theme: state.theme === "light" ? "dark" : "light",
-//     };
-//   }
-
-//   if (action.type === "RESET_THEME") {
-//     return {
-//       theme: "light",
-//     };
-//   }
-
-//   return state;
-// };
-
-// const store = createStore(themeReducer);
-// export default store;
