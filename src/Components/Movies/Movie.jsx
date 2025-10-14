34f44f589cffaf2469f17ba7d1f40f9527387a2a
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2025-10-14 12:47:45 +0300

    Update Movie.jsx

--- src/Components/Movies/Movie.jsx
+++ src/Components/Movies/Movie.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { addFavourite } from "../../store/FavouriteSlice";
+import { useDispatch } from "react-redux";
 
-const Movie = ({ id, title, overview, poster_path }) => {
+const Movie = ({ id, title, overview, poster_path, movie }) => {
   const navigate = useNavigate();
 
   const handleNavigate = () => {
     navigate(`/movies/${id}`);
+  };
+  const dispatch = useDispatch();
+  const movieData = { id, title, overview, poster_path };
+
+  const addToFavourite = () => {
+    console.log(movieData);
+
+    dispatch(addFavourite(movieData));
   };
 
   return (
@@ -36,15 +46,36 @@ const Movie = ({ id, title, overview, poster_path }) => {
           {overview}
         </p>
       </div>
-
-      <button
-        onClick={handleNavigate}
-        className="hover:bg-[#3C467B] bg-[#134686] transition-all duration-300 
+      <div className=" flex flex-row-reverse justify-center items-center h-full w-full gap-2">
+        <button
+          onClick={() => addToFavourite(movie)}
+          className="bg-transparent mt-4  p-2 rounded-full hover:bg-[#3C467B] transition"
+        >
+          {/* <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="yellow"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="white"
+          className="w-5 h-5"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M21.8 8.25a6 6 0 00-9.8-6.9 6 6 0 00-9.8 6.9c3.1 5.5 9.8 11.2 9.8 11.2s6.7-5.7 9.8-11.2z"
+          />
+        </svg> */}
+          ❤️
+        </button>
+        <button
+          onClick={handleNavigate}
+          className="hover:bg-[#3C467B] bg-[#134686] transition-all duration-300 
                text-white font-semibold py-2 px-4 rounded-lg mt-4 
                w-full text-center"
-      >
-        View Details
-      </button>
+        >
+          View Details
+        </button>
+      </div>
     </li>
   );
 };
