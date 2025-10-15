825b6f4248b9f5c00191235709b789ee207f1c07
Gerges Nashaat Hosny <Georgios.Nashaat@gmail.com>
2025-10-15 11:38:07 +0300

    Update AxiosInstance.js

--- src/AxiosInstance.js
+++ src/AxiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
-import { store } from "./store/store";
-import { changeLoaderState } from "./store/LoaderSlice";
+// import { store } from "./store/store";
+// import { changeLoaderState } from "./store/LoaderSlice";
 
 const axiosInstance = axios.create({
   baseURL: "https://api.themoviedb.org/3",
@@ -9,23 +9,23 @@ const axiosInstance = axios.create({
 export default axiosInstance;
 axiosInstance.interceptors.request.use(
   (config) => {
-    store.dispatch(changeLoaderState(true));
+    // store.dispatch(changeLoaderState(true));
     return config;
   },
 
   (error) => {
-    store.dispatch(changeLoaderState(false));
+    // store.dispatch(changeLoaderState(false));
     Promise.reject(error);
   }
 );
 
 axiosInstance.interceptors.response.use(
   (response) => {
-    store.dispatch(changeLoaderState(false));
+    // store.dispatch(changeLoaderState(false));
     return response;
   },
   (error) => {
-    store.dispatch(changeLoaderState(false));
+    // store.dispatch(changeLoaderState(false));
     Promise.reject(error);
   }
 );
